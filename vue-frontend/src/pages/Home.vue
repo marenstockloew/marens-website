e61ed3706bf3cc25b0c919401b98f60f554be46a
marenstockloew <m.stockloew@posteo.de>
2024-12-12 22:41:14 +0000

    Added the rendering and backend of the home fields.

--- vue-frontend/src/pages/Home.vue
+++ vue-frontend/src/pages/Home.vue
@@ -1,6 +1,27 @@
 <template>
-
-  Maren Stocklöw
-  <a>works</a>
-
+  <div v-if="result" class="home-container">
+    <div v-html="result.entries[0].homeText"> </div>
+    <div>{{result.entries[0].homeHighlighted}}</div>
+  </div>
+  <div v-else>Maren Stocklöw</div>
 </template>
+
+<script setup>
+import { useQuery } from "@vue/apollo-composable";
+import { gql } from "graphql-tag";
+
+const { result } = useQuery(gql`
+query home {
+  entries(section: "home") {
+    id
+    title
+    url
+    ... on home_Entry{
+    homeText,
+      homeHighlighted
+    }
+  }
+}
+`);
+</script>
+
