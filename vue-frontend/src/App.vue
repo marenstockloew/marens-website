bf51b9fa4d9d93a11d8785c24a34703a6b8866c6
marenstockloew <m.stockloew@posteo.de>
2024-12-12 16:56:13 +0000

    Beginning of header

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,3 +1,12 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
+
+<script setup>
+
+import TheHeader from './components/TheHeader.vue';
+
+components: TheHeader;
+
+</script>
---
+++ vue-frontend/src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <ul>
+      <li><a>works</a></li>
+      <li><a>about</a></li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+ul {
+    width: 100vw;
+    margin-bottom: 5rem;
+    position: sticky;
+    top: 0;
+    list-style-type: none;
+    background-color: var(--dunkel);
+    color: var(--hellblau);
+    display: flex;
+    flex-direction: row;
+    font-size: 50px;
+}
+li {
+    margin: .5rem;
+}
+</style>
