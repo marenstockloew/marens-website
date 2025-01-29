497b74c00f4af6baade2feafd5894f7b56add98f
marenstockloew <m.stockloew@posteo.de>
2025-01-29 19:14:40 +0100

    Added a new variable with the query result for the workDetails component. Turned this into a computed variable.

--- vue-frontend/src/pages/Works/WorkDetails.vue
+++ vue-frontend/src/pages/Works/WorkDetails.vue
@@ -1,48 +1,108 @@
 <template>
-  <div v-if="result" class="content">
-    <h2>{{ result.entries[0].title }}</h2>
+  <div v-if="currentWorkDetails" class="content">
+    <h2>{{ currentWorkDetails.title }}</h2>
     <div class="text">
-      <img :src="result.entries[0].mainImage[0].url" class="mainImage" />
-      <p>{{ result.entries[0].description }}</p>
+      <img :src="currentWorkDetails.mainImage[0].url" class="mainImage" />
+      <p>{{ currentWorkDetails.description }}</p>
     </div>
   </div>
-  <div v-else class="content"> Could not find this artwork. <router-link :to="{name: 'Works'}">Go back to overview</router-link>.</div>
+  <div v-else class="content">
+    Could not find this artwork.
+    <router-link :to="{ name: 'Works' }">Go back to overview</router-link>.
+  </div>
 </template>
 
 <script setup>
 // https://apollo.vuejs.org/guide-composable/query
 // CAREFUL: Craft wants a [String] as variable type
-
+import { computed } from "vue";
 import { useQuery } from "@vue/apollo-composable";
 import { gql } from "graphql-tag";
 
 const props = defineProps(["slug"]);
-const currentSlug = props.slug;
+const currentSlugValue = props.slug;
 
-const { result, variables } = useQuery(
-  gql`
-    query workDetails($slug: [String]) {
-      entries(slug: $slug) {
-        id
-        title
-        slug
-        ... on works_Entry {
-          description
-          mainImage {
-            url
-          }
+const SELECTED_WORK_DETAILS_QUERY = gql`
+  query workDetails($slug: [String]) {
+    entries(slug: $slug) {
+      id
+      title
+      slug
+      ... on works_Entry {
+        description
+        mainImage {
+          title
+          url
         }
       }
     }
-  `
-);
+  }
+`;
+
+const { result, variables } = useQuery(SELECTED_WORK_DETAILS_QUERY);
+
 function selectCurrentSlug(slug) {
   variables.value = {
     slug,
   };
 }
 
-selectCurrentSlug(currentSlug);
+selectCurrentSlug(currentSlugValue);
+
+const currentWorkDetails = computed(() => result.value?.entries[0] ?? null);
+
+// watch(result, value => {
+//   currentWorkDetails = value;
+//   currentWorkDetails = currentWorkDetails.entries[0]
+//   console.log(currentWorkDetails);
+//     })
+
+// onResult(queryResult => {
+//   currentWorkDetails = queryResult.data;
+//   console.log(currentWorkDetails)
+// })
+
+// query workDetails($slug: [String]) {
+//   entries(slug: $slug) {
+//     id
+//     title
+//     slug
+//     ... on works_Entry {
+//       description
+//       mainImage {
+//         title
+//         url
+//       }
+//       details {
+//         ... on exhibitionDate_Entry {
+//           typeHandle
+//           title
+//           date
+//         }
+//         ... on additionalText_Entry {
+//           typeHandle
+//           title
+//           description
+//         }
+//         ... on photoDocumentation_Entry {
+//           typeHandle
+//           title
+//           photos {
+//             title
+//             url
+//           }
+//         }
+//         ... on videoDocumentation_Entry {
+//           typeHandle
+//           title
+//           video {
+//             url
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
 </script>
 
 <style scoped>
